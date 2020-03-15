f3b236d718e6f0c70aad354b7a938f2a6d797fc1
Bilal Taherkheli <bilal76@gmail.com>
2020-03-15 19:28:00 +0100

    added DTO class, and a temporary in-memory data store for now

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -1,24 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CityInfo.API.Controllers
 {
   [ApiController]
-  [Route("api/cities")]
+  [Route("api/cities/")]
   public class CitiesController : ControllerBase
   {
     [HttpGet]
     public JsonResult GetCities()
     {
-      return new JsonResult(
-        new List<object>()
-        {
-          new { id = 1, Name = "New York City" },
-          new { id = 2, Name = "Antwerp" }
-        });
+      return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+      return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault( c => c.Id == id));
     }
   }
 }
