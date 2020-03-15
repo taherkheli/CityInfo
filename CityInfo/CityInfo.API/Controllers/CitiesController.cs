28e2517f85776a567e5b8be988057564dd520f1a
Bilal Taherkheli <bilal76@gmail.com>
2020-03-15 16:01:34 +0100

    changed port to 1028 to match postman collection file. Added a controller for getting cities. Added attribute based routing for GetCities() -> /api/cities now sends the 2 test cities back whether invoked from browser or Postman

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+  [ApiController]
+  public class CitiesController : ControllerBase
+  {
+    [HttpGet("api/cities")]
+    public JsonResult GetCities()
+    {
+      return new JsonResult(
+        new List<object>()
+        {
+          new { id = 1, Name = "New York City" },
+          new { id = 2, Name = "Antwerp" }
+        });
+    }
+  }
+}
