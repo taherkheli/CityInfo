bb5698815a0a2d59a8e6762fbe0d9c2baa7a1e1b
Bilal Taherkheli <bilal@danfoss.com>
2020-03-18 18:21:30 +0100

    Adding possibility for seeding initial data. Created some seed data via overriding OnModelCreating() in DBContext and ran a migration. SQL tables now has the data populated. Mod6 completed.

--- CityInfo.API/Contexts/CityInfoContext.cs
+++ CityInfo.API/Contexts/CityInfoContext.cs
@@ -23,5 +23,76 @@ namespace CityInfo.API.Contexts
 			//Migrations will take care of creating the DB 
 			//Database.EnsureCreated();   //if DB exists already this will do nothing
 		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<City>()
+				.HasData(
+				new City()
+				{
+					Id = 1,
+					Name = "New York City",
+					Description = "The one with that big park."
+				},
+				new City()
+				{
+					Id = 2,
+					Name = "Antwerp",
+					Description = "The one with the cathedral that was never really finished."
+				},
+				new City()
+				{
+					Id = 3,
+					Name = "Paris",
+					Description = "The one with that big tower."
+				});
+
+			modelBuilder.Entity<POI>()
+			.HasData(
+				new POI()
+				{
+					Id = 1,
+					CityId = 1,
+					Name = "Central Park",
+					Description = "The most visited urban park in the United States."
+				},
+				new POI()
+				{
+					Id = 2,
+					CityId = 1,
+					Name = "Empire State Building",
+					Description = "A 102-story skyscraper located in Midtown Manhattan."
+				},
+				new POI()
+				{
+					Id = 3,
+					CityId = 2,
+					Name = "Cathedral",
+					Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans."
+				},
+				new POI()
+				{
+					Id = 4,
+					CityId = 2,
+					Name = "Antwerp Central Station",
+					Description = "The the finest example of railway architecture in Belgium."
+				},
+				new POI()
+				{
+					Id = 5,
+					CityId = 3,
+					Name = "Eiffel Tower",
+					Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel."
+				},
+				new POI()
+				{
+					Id = 6,
+					CityId = 3,
+					Name = "The Louvre",
+					Description = "The world's largest museum."
+				});
+
+			base.OnModelCreating(modelBuilder);
+		}
 	}
 }
