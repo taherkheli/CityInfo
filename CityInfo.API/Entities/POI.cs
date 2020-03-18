ee8f153d6960fd38e4460640bb023a6e520488d3
Bilal Taherkheli <bilal@danfoss.com>
2020-03-18 13:39:21 +0100

    Created entity classes with appropriate attributes and navigation properties to prepare for introducing EF Core later

---
+++ CityInfo.API/Entities/POI.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+	public class POI
+	{
+		[Key] //by convention it aint needed but explicit > implicit
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		//public string Description { get; set; }
+
+		[ForeignKey("CityId")]    //by convention it aint needed but explicit > implicit
+		public City City { get; set; }
+
+		public int CityId { get; set; }
+	}
+}
