f1bf303796e44bb7b1a045cd84b0e7adbef757ab
Bilal Taherkheli <bilal@danfoss.com>
2020-03-18 17:27:12 +0100

    Got rid of hardcoded connStr by moving it to appsettings. When deploying we will have a real SQL server elsewhere and cpnnection string would be even more sensitive. One can edit launchSettings.json to provide a connection string for prod env but practically it would be some kind of azure keyvault thingy that will be realistic

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -4,12 +4,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CityInfo.API
 {
   public class Startup
   {
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+      _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
@@ -28,12 +37,12 @@ namespace CityInfo.API
       //                });
 
       services.AddTransient<IMailService, LocalMailService>();
-      
-      var ConnStr = @"Server=(localdb)\MSSQLLocalDB; Database = CityInfoDB; Trusted_Connection = true;";
+
+      var connStr = _configuration["connectionString:cityInfoDBConnectionString"];
 
       //scoped lifetime by default
       services.AddDbContext<CityInfoContext>(o => {
-        o.UseSqlServer(ConnStr);
+        o.UseSqlServer(connStr);
       });  
     }
 
